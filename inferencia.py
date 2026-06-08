01936a9982f292f5d12ff18e465f16e1b9d5a1b3
Galakyta <galakyta@gmail.com>
2026-06-08 20:16:02 -0300

    felizmente rodou XESUS

--- inferencia.py
+++ inferencia.py
@@ -1,7 +1,9 @@
 from pickle import load
 import pandas as pd
 
-modelo = load(open("rf_credit.pkl", "rb"))
+modelo = load(open("CARTAO_rf.pkl", "rb"))
+
+# loadando o modelo normalmente 
 
 colunas = [
     "LIMIT_BAL", "SEX", "EDUCATION", "MARRIAGE", "AGE",
@@ -10,7 +12,10 @@ colunas = [
     "PAY_AMT1", "PAY_AMT2", "PAY_AMT3", "PAY_AMT4", "PAY_AMT5", "PAY_AMT6"
 ]
 
-# substitua pelos valores reais do cliente
+# cuidado pra nn colocar id de novo aq pq a gente dropo antes pra n gerar a hierarquia
+
+# aq da pra substituir por qualquer dado, mas eu gerei esses ai pra fatura pra ficar mais facil
+#na teoria com essa quantidade de atraso deve atrasar bem 
 novo_cliente = pd.DataFrame([{
     "LIMIT_BAL": 20000,
     "SEX": 2,
@@ -37,9 +42,18 @@ novo_cliente = pd.DataFrame([{
     "PAY_AMT6": 0
 }], columns=colunas)
 
+novo_cliente = pd.get_dummies(novo_cliente, drop_first=True)
+
+novo_cliente = novo_cliente.reindex(columns=modelo.feature_names_in_, fill_value=0)
+
 pred = modelo.predict(novo_cliente)[0]
 proba = modelo.predict_proba(novo_cliente)[0]
 
-print(f"Predicao: {'default' if pred == 1 else 'nao default'}")
-print(f"Probabilidade nao default: {proba[0]:.2%}")
-print(f"Probabilidade default:     {proba[1]:.2%}")
+print(f"Predicao:{'default' if pred == 1 else 'nao default'}")
+print(f"Probabilidade nao default (pagar em dia):  {proba[0]:.2%}")
+print(f"Probabilidade default (defaultar):  {proba[1]:.2%}") #atrasar e nunca mais sair do cheque especial
+
+# Predicao:default
+#Probabilidade nao default (pagar em dia):  11.00%
+#Probabilidade default (defaultar):  89.00%
+#PS C:\Users\galak\OneDrive\Desktop\A2-2-SISTEMAS-INT>'''
